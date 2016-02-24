ac2d45329bc1f1b9b160828c38b4e74ea49af0bf
emesika <emesika@redhat.com>
2016-02-24 14:59:49 +0200

    Eli Mesika

--- string_ext.rb
+++ string_ext.rb
@@ -1,2 +1,17 @@
+
 class StringExt
+    def initialize(f,s)
+        @@first=f
+        @@second=s
+    end
+
+    def concat()
+        @@first + @@second
+    end
+
+    def substract()
+        @@first[@@second] = ""
+        @@first
+    end
+
 end
